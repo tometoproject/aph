50136af53ce05c6f32bfc37f747d9c39f4831cf5
marisa <mokou@posteo.de>
2019-12-03 17:34:09 +0100

    Remove unneeded Plugs

--- lib/aph_web/endpoint.ex
+++ lib/aph_web/endpoint.ex
@@ -1,14 +1,6 @@
 defmodule AphWeb.Endpoint do
   use Phoenix.Endpoint, otp_app: :aph
 
-  socket "/socket", AphWeb.UserSocket,
-    websocket: true,
-    longpoll: false
-
-  # Serve at "/" the static files from "priv/static" directory.
-  #
-  # You should set gzip to true if you are running phx.digest
-  # when deploying your static files in production.
   plug Plug.Static,
     at: "/",
     from: :aph,
@@ -31,14 +23,6 @@ defmodule AphWeb.Endpoint do
 
   plug Plug.MethodOverride
   plug Plug.Head
-
-  # The session will be stored in the cookie and signed,
-  # this means its contents can be read but not tampered with.
-  # Set :encryption_salt if you would also like to encrypt it.
-  plug Plug.Session,
-    store: :cookie,
-    key: "_aph_key",
-    signing_salt: "kjRL8dvd"
 
   plug CORSPlug
 
